c070deef2019d2dda1c83d097519b1b764682d11
Simon Gao <simon.shubin@gmail.com>
2024-10-24 14:51:46 +0200

    ✨ Added basic endpoints

--- src/main/java/dezsys/authentication/Controller.java
+++ src/main/java/dezsys/authentication/Controller.java
@@ -1,4 +1,24 @@
+package dezsys.authentication;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class Controller {
-    
+
+    @RequestMapping("/admin/register")
+    public String register() {
+        return "register";
+    }
+
+    @RequestMapping("/signin")
+    public String signin() {
+        return "signin";
+    }
+
+    @RequestMapping("/verify")
+    public String verify() {
+        return "verify";
+    }
 }
