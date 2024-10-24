14f206cad0143165b455e7fae18428171ec95431
Simon Gao <simon.shubin@gmail.com>
2024-10-24 16:24:34 +0200

    ✨ Added setter and getter for user

--- src/main/java/dezsys/authentication/User.java
+++ src/main/java/dezsys/authentication/User.java
@@ -17,4 +17,52 @@ public class User {
     public String email;
     public List<Role> roles;
     public String password;
+
+    public User(Long id, String name, String email, List<Role> roles, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.roles = roles;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
