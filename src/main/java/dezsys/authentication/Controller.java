69170a3bf8a2e1af3f16d0f5f65f22c26bf50b7d
Simon Gao <simon.shubin@gmail.com>
2024-10-24 15:59:22 +0200

    ✨ Added basic jwt

--- src/main/java/dezsys/authentication/Controller.java
+++ src/main/java/dezsys/authentication/Controller.java
@@ -1,22 +1,31 @@
 package dezsys.authentication;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 import java.util.stream.Collectors;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+
 @RestController
 @RequestMapping("/auth")
 public class Controller {
+    private SecretKey key = Jwts.SIG.HS256.key().build();
 
     @RequestMapping("/admin/register")
-    public String register(String name, String email, @RequestParam(value = "roles")ArrayList<Role> roles) {
+    public String register(String name, String email, @RequestParam(value = "roles") ArrayList<Role> roles, String password) {
         System.out.println(String.format("name: %s, email: %s, roles: %s", name, email,
                 roles.stream().map(Role::name).collect(Collectors.joining(", "))));
-        return "register";
+        return createJwt(email, password);
     }
 
     @RequestMapping("/signin")
@@ -25,7 +34,34 @@ public class Controller {
     }
 
     @RequestMapping("/verify")
-    public String verify() {
+    public String verify(String token) {
+        System.out.println("token: " + token);
+        parseJwt(token);
         return "verify";
     }
+
+
+    public String createJwt(String email, String password) {
+
+        return Jwts.builder()
+                .claim("id", email)
+                .claim("password", password)
+                .issuer("server")
+                .issuedAt(Date.from(Instant.now()))
+                .expiration(Date.from(Instant.now().plus(10, ChronoUnit.MINUTES)))
+                .signWith(key)
+                .compact();
+    }
+
+    public Jws<Claims> parseJwt(String jwtString) {
+        Jws<Claims> jwt = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(jwtString);
+        System.out.println(jwt.getPayload().get("id"));
+        System.out.println(jwt.getPayload().get("password"));
+
+        return jwt;
+    }
+
 }
