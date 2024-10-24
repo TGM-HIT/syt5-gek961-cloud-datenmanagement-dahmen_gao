341de090a3e535249dbee77fe3f66d03e7a4b906
Simon Gao <simon.shubin@gmail.com>
2024-10-24 14:32:49 +0200

    🎉 Initial commit

---
+++ src/main/java/dezsys/authentication/Dezsys1Application.java
@@ -0,0 +1,13 @@
+package dezsys.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dezsys1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dezsys1Application.class, args);
+	}
+
+}
